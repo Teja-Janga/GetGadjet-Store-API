38267458a625d4feb785be752266c34cbcf72da6
Teja Janga <tejat7927@gmail.com>
2026-02-20 17:34:45 +0530

    Updated database.php file

--- database.php
+++ database.php
@@ -1,13 +1,14 @@
 
 <?php
-    $db_server = "localhost";
-    $db_user = "root";
-    $db_pass = "";
-    $db_name = "getgadjet";
+    $db_server = "mysql-3a1ea88d-tejat7927-2604.b.aivencloud.com";
+    $db_user = "avnadmin";
+    $db_pass = "AVNS__N9ORxGkKT8wybBBTgz";
+    $db_name = "defaultdb";
+    $port = "27293";
     $conn = "";     // Connection Variable
 
     try {
-        $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+        $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name, $port);
 
         // To handle special characters correctly
         mysqli_set_charset($conn, "utf8mb4");
